f9d10da4c05a48ab22dd58d2dabb1e333600cb08
Saurabh Deshmukh <saurabhd380@gmail.com>
2020-01-27 11:40:04 +0530

    msg

---
+++ trremap4.java
@@ -0,0 +1,17 @@
+//Write a Java program to remove all the mappings from a map
+import java.util.*;  
+public class Example4 {  
+   public static void main(String args[]) {
+  HashMap <Integer,String> hash_map = new HashMap <Integer,String> ();
+  hash_map.put(1, "Red");
+  hash_map.put(2, "Green");
+  hash_map.put(3, "Black");
+  hash_map.put(4, "White");
+  hash_map.put(5, "Blue");
+  // print the map
+  System.out.println("The Original linked map: " + hash_map);
+  // Removing all the elements from the linked map
+  hash_map.clear();
+  System.out.println("The New map: " + hash_map);
+ }
+}
